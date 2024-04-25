bf8f2a837abcfa42d8b34c06b73a3dc2bffe80b2
vovantus <vshirokov@gmail.com>
2024-04-25 21:10:25 +0200

    florist shop loader fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,39 @@ import URLS from "./routes/routes.ts";
 import CartPage from "./pages/CartPage/CartPage.tsx";
 import NewsPage from "./pages/NewsPage/NewsPage.tsx";
 import Page404 from "./pages/NotFoundPage/Page404.tsx";
+import CategoriesPage from "./pages/CategoriesPage/CategoriesPage.tsx";
 import { useGetFloristInfo } from "./hooks/useGetFloristInfo.ts";
-
+import useSubdomain from "./hooks/useSubdomain.ts";
+import ShopMainPageSkeleton from "./pages/ShopMainPage/ShopMainPageSkeleton.tsx";
 
 // ask: 24 04 долго грузится, сначала показыват 404 при первом обращении
 
 function App() {
   const { floristInfo } = useGetFloristInfo();
+  const { subdomain } = useSubdomain();
 
-  return floristInfo ? (
+  return subdomain ? (
     <Routes>
-      <Route element={<FloristShopLayout />}>
-        <Route
-          path={URLS.ROOT}
-          element={<ShopMainPage florist={floristInfo.name} />}
-        />
-        <Route path={URLS.FLORIST.NEWS} element={<NewsPage />} />
-        <Route
-          path={URLS.FLORIST.CART}
-          element={<CartPage florist={floristInfo.name} />}
-        />
-        <Route path="*" element={<Page404 />} />
-      </Route>
+      {floristInfo ? (
+        <Route element={<FloristShopLayout />}>
+          <Route
+            path={URLS.ROOT}
+            element={<ShopMainPage florist={floristInfo.name} />}
+          />
+          <Route
+            path={URLS.FLORIST.CATALOG}
+            element={<CategoriesPage florist={floristInfo.name} />}
+          />
+          <Route path={URLS.FLORIST.NEWS} element={<NewsPage />} />
+          <Route
+            path={URLS.FLORIST.CART}
+            element={<CartPage florist={floristInfo.name} />}
+          />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      ) : (
+        <Route path="*" element={<ShopMainPageSkeleton />} />
+      )}
     </Routes>
   ) : (
     <Routes>
